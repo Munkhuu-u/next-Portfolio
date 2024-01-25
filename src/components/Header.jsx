33e2ed206c44b638fa40edceae296482785b1a19
Munkhuu-u <152163557+Munkhuu-u@users.noreply.github.com>
2024-01-25 09:38:59 +0800

    done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -23,7 +23,7 @@ export const Header = () => {
             </a>
           </div>
           <div>
-            <div className="sm:flex sm:flex-row sm:visible flex h-fit hidden">
+            <div className="sm:flex sm:flex-row sm:visible flex h-fit">
               <div className="sm:flex sm:flex-row sm:gap-6 sm:mr-4 sm:items-center sm:visible flex collapse">
                 <a href="#About" scroll={true}>
                   About
@@ -72,86 +72,86 @@ export const Header = () => {
         </div>
       </div>
       {/* Hamburger menu section */}
-      {showMenu && (
-        <div className=" ">
-          {/* Menu rear division */}
-          <div
-            className={`absolute left-0 top-0 w-full h-full bg-gray-500 bg-opacity-50 transition-all duration-300  ease-in-out`}
-            onClick={(e) => {
-              setShowMenu(!showMenu);
-            }}
-          ></div>
+      {/* {showMenu && ( */}
+      <div
+        className={`absolute left-0 top-0 ${
+          showMenu ? "w-full h-full" : "w-0 h-0"
+        } bg-gray-500 bg-opacity-50 transition-all duration-300 linear`}
+        onClick={(e) => {
+          setShowMenu(!showMenu);
+        }}
+      >
+        {/* Menu rear division */}
+        <div></div>
+        {/* Front */}
+        <div
+          className={`w-4/5 h-full absolute top-0 right-0 z-20 overflow-hidden ${
+            theme == "dark" ? "bg-gray-950" : "bg-gray-50"
+          }`}
+        >
+          {/* header */}
+          <div className="flex flex-row justify-between items-center p-4">
+            <a href="#0">
+              <Logo />
+            </a>
+            <button
+              onClick={() => {
+                setShowMenu(!showMenu);
+              }}
+            >
+              <Esc />
+            </button>
+          </div>
 
-          {/* Front */}
+          {/* Options */}
           <div
-            className={`w-4/5 h-full absolute top-0 right-0 z-20 overflow-hidden ${
-              theme == "dark" ? "bg-gray-950" : "bg-gray-50"
-            }`}
+            className={`${
+              theme == "dark" ? "text-gray-300" : "text-gray-600"
+            } border-y flex flex-col w-full p-4 gap-4 font-medium`}
           >
-            {/* header */}
-            <div className="flex flex-row justify-between items-center p-4">
-              <a href="#0">
-                <Logo />
-              </a>
+            <a href="#About" scroll={true}>
+              About
+            </a>
+            <a href="#Experience" scroll={true}>
+              Testimonials
+            </a>
+            <a href="#Work" scroll={true}>
+              Work
+            </a>
+            <a href="#Contact" scroll={true}>
+              Contact
+            </a>
+          </div>
+
+          {/* theme */}
+          <div className="flex flex-col p-4 gap-4 w-full  ">
+            <div className="flex flex-row justify-between">
+              <p>Switch theme</p>
               <button
                 onClick={() => {
-                  setShowMenu(!showMenu);
+                  setTheme(theme === "light" ? "dark" : "light");
+                  console.log(theme);
                 }}
               >
-                <Esc />
+                {theme === "dark" ? <Moon /> : <Sun />}
               </button>
             </div>
 
-            {/* Options */}
-            <div
+            {/* download */}
+            <button
               className={`${
-                theme == "dark" ? "text-gray-300" : "text-gray-600"
-              } border-y flex flex-col w-full p-4 gap-4 font-medium`}
+                theme === "light"
+                  ? "bg-black text-white"
+                  : "bg-white text-black"
+              } rounded-xl px-[16px] py-[6px] text-gray-900 text-base font-medium`}
             >
-              <a href="#About" scroll={true}>
-                About
-              </a>
-              <a href="#Experience" scroll={true}>
-                Testimonials
-              </a>
-              <a href="#Work" scroll={true}>
-                Work
-              </a>
-              <a href="#Contact" scroll={true}>
-                Contact
-              </a>
-            </div>
-
-            {/* theme */}
-            <div className="flex flex-col p-4 gap-4 w-full  ">
-              <div className="flex flex-row justify-between">
-                <p>Switch theme</p>
-                <button
-                  onClick={() => {
-                    setTheme(theme === "light" ? "dark" : "light");
-                    console.log(theme);
-                  }}
-                >
-                  {theme === "dark" ? <Moon /> : <Sun />}
-                </button>
-              </div>
-
-              {/* download */}
-              <button
-                className={`${
-                  theme === "light"
-                    ? "bg-black text-white"
-                    : "bg-white text-black"
-                } rounded-xl px-[16px] py-[6px] text-gray-900 text-base font-medium`}
-              >
-                Download CV
-              </button>
-            </div>
-
-            <div className="h-screen"></div>
+              Download CV
+            </button>
           </div>
+          <div className="h-screen"></div>
         </div>
-      )}
+      </div>
+      {/* // )} */}
     </div>
   );
 };
