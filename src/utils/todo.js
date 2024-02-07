5068448f6606f78143d599bf900e956e706ad1f3
Munkhuu-u <152163557+Munkhuu-u@users.noreply.github.com>
2024-02-07 11:18:15 +0800

    added list on todo.js

--- src/utils/todo.js
+++ src/utils/todo.js
@@ -1,10 +1,14 @@
-//------TO DO--------
+//------TO DO LISTS--------
+
+//----DONE----
 //theme
 //response
-
 //hanburger: belen tailwind menu or use transition and position
+//scroll: Link
+
+//----Remaining----
+//Header section position
+//hanburger: transition
 //download cv: input choose documen
 //copied icon:
-//copy: google it
-
-//scroll: useRef
+//copy to clipboard: google it
